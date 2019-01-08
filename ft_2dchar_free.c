e2808386e67fb95cbf8a3edebabd1c058b82e55b
Maxence GRAB <magrab@e3r8p6.42.fr>
2019-01-08 20:43:42 +0100

    Im a star

--- ft_2dchar_free.c
+++ ft_2dchar_free.c
@@ -6,7 +6,7 @@
 /*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 17:48:25 by magrab            #+#    #+#             */
-/*   Updated: 2019/01/08 20:01:20 by magrab           ###   ########.fr       */
+/*   Updated: 2019/01/08 20:41:55 by magrab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ int		ft_2dchar_free(char ***map)
 {
 	int x;
 
-	if (!map || !(*map))
+	if (!map || !(map[0]))
 		return (0);
 	x = 0;
-	while (*map[x])
+	while (map[0][x])
 	{
-		free(*map[x]);
+		free(map[0][x]);
 		x++;
 	}
-	free(*map);
-	*map = NULL;
+	free(map[0]);
+	map[0] = NULL;
 	return (0);
 }
