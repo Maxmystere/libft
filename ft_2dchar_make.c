80b83f30acb1e2adc4d881331317c036ff779a2d
Maxence GRAB <magrab@e3r7p3.42.fr>
2019-01-15 16:29:03 +0100

    free if malloc error

--- ft_2dchar_make.c
+++ ft_2dchar_make.c
@@ -6,7 +6,7 @@
 /*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 17:33:36 by magrab            #+#    #+#             */
-/*   Updated: 2019/01/08 18:03:15 by magrab           ###   ########.fr       */
+/*   Updated: 2019/01/15 16:24:40 by magrab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ char		**ft_2dchar_make(int x, int y, char c)
 	while (v < x)
 	{
 		if (!(map[v] = ft_strnew_set(c, y)))
+		{
+			ft_2dchar_free(&map);
 			return (NULL);
+		}
 		v++;
 	}
 	return (map);
