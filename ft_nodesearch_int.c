e431c09e2abe380efdbaeb91b0da2e6498a2d657
Maxence GRAB <magrab@e2r10p20.42.fr>
2019-02-16 13:11:23 +0100

    Update function name

--- ft_nodesearch_int.c
+++ ft_nodesearch_int.c
@@ -6,13 +6,13 @@
 /*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:24:37 by tferrieu          #+#    #+#             */
-/*   Updated: 2019/02/14 16:24:38 by tferrieu         ###   ########.fr       */
+/*   Updated: 2019/02/16 13:08:16 by magrab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_tab	ft_nodesearch(t_tab node, t_type elem)
+t_tab	ft_nodesearch_int(t_tab node, t_type elem)
 {
 	t_tab pos;
 
